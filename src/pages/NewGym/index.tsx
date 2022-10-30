aaf7cdac9110f63ec4d8445b2471d4b873b2e68d
Diego Arruda <arrudadiego45@gmail.com>
2022-10-30 17:31:09 -0300

    Toasts

--- src/pages/NewGym/index.tsx
+++ src/pages/NewGym/index.tsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
 import isEmailValid from '../../utils/isEmailValid';
 import { isSomeEmpty } from '../../utils/isSomeEmpty';
 import { isNumeric } from '../../utils/isNumber';
+
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export default function NewGym() {
   const config = {
@@ -46,6 +49,12 @@ export default function NewGym() {
     setZipCode('');
   }
 
+  function createdToast() {
+    toast.success('Criado com sucesso !', {
+      position: toast.POSITION.BOTTOM_CENTER,
+    });
+  }
+
   async function handleCreateGym() {
     const someFieldIsEmpty = isSomeEmpty([name, currentCapacity, maxCapacity, email, password,
       zipCode, state, city, street, adressNumber]);
@@ -72,19 +81,26 @@ export default function NewGym() {
     }
     setZipCodeErr(false);
 
-    await axios.post('https://trainya-app-api.herokuapp.com/gyms', {
-      name,
-      currentCapacity: Number(currentCapacity),
-      maxCapacity: Number(currentCapacity),
-      email,
-      password,
-      zipCode: Number(zipCode),
-      state,
-      city,
-      street,
-      adressNumber: Number(adressNumber),
-    }, config);
-    return clearInputs();
+    try {
+      await axios.post('https://trainya-app-api.herokuapp.com/gyms', {
+        name,
+        currentCapacity: Number(currentCapacity),
+        maxCapacity: Number(currentCapacity),
+        email,
+        password,
+        zipCode: Number(zipCode),
+        state,
+        city,
+        street,
+        adressNumber: Number(adressNumber),
+      }, config);
+    } catch (error: any) {
+      return toast.error(error.response.data.message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+      });
+    }
+    clearInputs();
+    return createdToast();
   }
 
   if (zipCode.length >= 8) {
@@ -98,150 +114,114 @@ export default function NewGym() {
   });
 
   return (
-    <div className="flex justify-center mt-0">
-      <div className="w-full max-w-3xl flex items-center justify-center flex-col bg-[#05142D] border border-gray-800 rounded-2xl text-white px-12 py-8">
-        <div className="flex w-full items-center border-b-[0.5px] border-gray-800 pb-4 justify-between">
-          <h1 className="font-bold text-3xl">Cadastrar academias</h1>
-          <Link to="/List" className="border-2 p-2 border-gray-500 bg-[#0b1d3d] hover:scale-[1.01] hover:bg-[#13284c] rounded-lg">
-            Ver academias
-          </Link>
-        </div>
-        <div className="flex flex-row gap-1 text-sm items-center opacity-50 mt-1 mb-6" />
-        <div className="flex gap-2 flex-row w-full mb-7">
-          <div className="w-full flex flex-col gap-1">
-            <span>Nome da academia *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              value={name}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
+    <>
+      <div className="flex justify-center mt-0">
+        <div className="w-full max-w-3xl flex items-center justify-center flex-col bg-[#05142D] border border-gray-800 rounded-2xl text-white px-12 py-8">
+          <div className="flex w-full items-center border-b-[0.5px] border-gray-800 pb-4 justify-between">
+            <h1 className="font-bold text-3xl">Cadastrar academias</h1>
+            <Link to="/List" className="border-2 p-2 border-gray-500 bg-[#0b1d3d] hover:scale-[1.01] hover:bg-[#13284c] rounded-lg">
+              Ver academias
+            </Link>
           </div>
-        </div>
-        <div className="flex gap-2 md:flex-row w-full mb-7 flex-col">
-          <div className="md:w-2/4 w-full flex flex-col gap-1">
-            <span>Capacidade atual *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setCurrentCapacity(e.target.value);
-              }}
-              value={currentCapacity}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
+          <div className="flex flex-row gap-1 text-sm items-center opacity-50 mt-1 mb-6" />
+          <div className="flex gap-2 flex-row w-full mb-7">
+            <div className="w-full flex flex-col gap-1">
+              <span>Nome da academia *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+                value={name}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
           </div>
-          <div className="md:w-2/4 w-full flex flex-col gap-1">
-            <span>Capacidade máxima *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setMaxCapacity(e.target.value);
-              }}
-              value={maxCapacity}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
+          <div className="flex gap-2 md:flex-row w-full mb-7 flex-col">
+            <div className="md:w-2/4 w-full flex flex-col gap-1">
+              <span>Capacidade atual *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setCurrentCapacity(e.target.value);
+                }}
+                value={currentCapacity}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
+            <div className="md:w-2/4 w-full flex flex-col gap-1">
+              <span>Capacidade máxima *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setMaxCapacity(e.target.value);
+                }}
+                value={maxCapacity}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex gap-2 md:flex-row w-full mb-7 flex-col">
-          <div className="md:w-2/4 w-full flex flex-col gap-1">
-            <span>E-mail *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              value={email}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
+          <div className="flex gap-2 md:flex-row w-full mb-7 flex-col">
+            <div className="md:w-2/4 w-full flex flex-col gap-1">
+              <span>E-mail *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                value={email}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
+            <div className="md:w-2/4 w-full flex flex-col gap-1">
+              <span>Senha *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                value={password}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
           </div>
-          <div className="md:w-2/4 w-full flex flex-col gap-1">
-            <span>Senha *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              value={password}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
+          <div className="flex gap-2 md:flex-row w-full mb-7 flex-col">
+            <div className="md:w-2/4 w-full flex flex-col gap-1">
+              <span>CEP *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setZipCode(e.target.value);
+                }}
+                value={zipCode}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
+            <div className="md:w-2/4 w-full flex flex-col gap-1">
+              <span>Estado *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setState(e.target.value);
+                }}
+                value={state}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
           </div>
-        </div>
-        <div className="flex gap-2 md:flex-row w-full mb-7 flex-col">
-          <div className="md:w-2/4 w-full flex flex-col gap-1">
-            <span>CEP *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setZipCode(e.target.value);
-              }}
-              value={zipCode}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
-          </div>
-          <div className="md:w-2/4 w-full flex flex-col gap-1">
-            <span>Estado *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setState(e.target.value);
-              }}
-              value={state}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
-          </div>
-        </div>
-        <div className="gap-2 flex-row w-full mb-7 hidden md:flex">
-          <div className="w-2/5 flex flex-col gap-1">
-            <span>Cidade *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setCity(e.target.value);
-              }}
-              value={city}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
-          </div>
-          <div className="w-2/5 flex flex-col gap-1">
-            <span>Endereço *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setStreet(e.target.value);
-              }}
-              value={street}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
-          </div>
-          <div className="w-1/5 flex flex-col gap-1">
-            <span>Número *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setAdressNumber(e.target.value);
-              }}
-              value={adressNumber}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
-          </div>
-        </div>
-
-        <div className="gap-2 flex-col w-full mb-7 flex md:hidden">
-          <div className="w-full flex flex-col gap-1">
-            <span>Cidade *</span>
-            <input
-              type="text"
-              onChange={(e) => {
-                setCity(e.target.value);
-              }}
-              value={city}
-              className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
-            />
-          </div>
-          <div className="flex flex-row gap-2">
-            <div className="w-3/4 flex flex-col gap-1">
+          <div className="gap-2 flex-row w-full mb-7 hidden md:flex">
+            <div className="w-2/5 flex flex-col gap-1">
+              <span>Cidade *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }}
+                value={city}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
+            <div className="w-2/5 flex flex-col gap-1">
               <span>Endereço *</span>
               <input
                 type="text"
@@ -252,8 +232,8 @@ export default function NewGym() {
                 className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
               />
             </div>
-            <div className="w-1/4 flex flex-col gap-1">
-              <span>Num. *</span>
+            <div className="w-1/5 flex flex-col gap-1">
+              <span>Número *</span>
               <input
                 type="text"
                 onChange={(e) => {
@@ -264,17 +244,57 @@ export default function NewGym() {
               />
             </div>
           </div>
-        </div>
-        {empty && <span className="text-red-500 mb-4">Preencha todos os campos e tente novamente!</span>}
-        {zipCodeErr && <span className="text-red-500 mb-4">Digite um CEP válido.</span>}
-        {emailErr && <span className="text-red-500 mb-4">O formato do e-mail não é válido.</span>}
-        {notNumber && <span className="text-red-500 mb-4">Os campos de Capacidade, CEP e Número precisam ser números.</span>}
-        <div className="w-full bg-[#1753B2] hover:bg-[#2960b9] active:bg-[#0d4cb0] py-3 px-4 font-semibold text-lg rounded-lg">
-          <button type="submit" onClick={handleCreateGym} className="w-full  flex items-center justify-center">
-            <span className="opacity-90">Cadastrar</span>
-          </button>
+
+          <div className="gap-2 flex-col w-full mb-7 flex md:hidden">
+            <div className="w-full flex flex-col gap-1">
+              <span>Cidade *</span>
+              <input
+                type="text"
+                onChange={(e) => {
+                  setCity(e.target.value);
+                }}
+                value={city}
+                className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+              />
+            </div>
+            <div className="flex flex-row gap-2">
+              <div className="w-3/4 flex flex-col gap-1">
+                <span>Endereço *</span>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setStreet(e.target.value);
+                  }}
+                  value={street}
+                  className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+                />
+              </div>
+              <div className="w-1/4 flex flex-col gap-1">
+                <span>Num. *</span>
+                <input
+                  type="text"
+                  onChange={(e) => {
+                    setAdressNumber(e.target.value);
+                  }}
+                  value={adressNumber}
+                  className="bg-transparent py-3 px-4 font-semibold w-full outline-0 border-[1px] focus:border-[#2176FF] border-gray-800  rounded-xl"
+                />
+              </div>
+            </div>
+          </div>
+          {empty && <span className="text-red-500 mb-4">Preencha todos os campos e tente novamente!</span>}
+          {zipCodeErr && <span className="text-red-500 mb-4">Digite um CEP válido.</span>}
+          {emailErr && <span className="text-red-500 mb-4">O formato do e-mail não é válido.</span>}
+          {notNumber && <span className="text-red-500 mb-4">Os campos de Capacidade, CEP e Número precisam ser números.</span>}
+          <div className="w-full bg-[#1753B2] hover:bg-[#2960b9] active:bg-[#0d4cb0] py-3 px-4 font-semibold text-lg rounded-lg">
+            <button type="submit" onClick={handleCreateGym} className="w-full  flex items-center justify-center">
+              <span className="opacity-90">Cadastrar</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <button onClick={createdToast} type="submit">Enviar</button>
+      <ToastContainer autoClose={2000} />
+    </>
   );
 }
